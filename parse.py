15dee764d9d82b606a2156ac41f8678eafadedc4
GidiZisk <gidonzisk@gmail.com>
2024-02-07 01:56:27 +0200

    Update parse.py

--- parse.py
+++ parse.py
@@ -3,14 +3,21 @@ import yaml
 def main():
     print("got here")
     yaml_name_before = sys.argv[1]
-    yaml_name_after = ""
-    if len(sys.argv) > 2:
-      yaml_name_after = sys.argv[2]
-    with open(yaml_name_before, "r") as file:
-          bla= file.read()
+    yaml_name_after = sys.argv[2]
 
-    with open(f"123.{yaml_name_before}", "w") as file:
-          file.write(bla)
+    if yaml_name_before:
+        with open(yaml_name_before, "r") as file:
+              bla= file.read()
+
+        with open(f"123.{yaml_name_before}", "w") as file:
+              file.write(bla)
+
+    if yaml_name_after:
+        with open(yaml_name_after, "r") as file:
+              bla= file.read()
+
+        with open(f"456.{yaml_name_after}", "w") as file:
+              file.write(bla)
     
 
 
