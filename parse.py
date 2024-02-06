8ce5d225baaf1bcfc71c606aa270d100687890e1
GidiZisk <gidonzisk@gmail.com>
2024-02-07 00:36:32 +0200

    Update parse.py

--- parse.py
+++ parse.py
@@ -6,9 +6,6 @@ def main():
     yaml_name_after = ""
     if len(sys.argv) > 2:
       yaml_name_after = sys.argv[2]
-      with open(yaml_name_after, "r") as file:
-          print(file.read())
-    print(yaml_name_before)
     with open(yaml_name_before, "r") as file:
           bla= file.read()
 
